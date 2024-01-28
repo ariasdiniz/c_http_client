2bfbd0ba3fef98d7383aa6c6e336db7cced7f87a
Aria Diniz <aria.diniz.dev@gmail.com>
2024-01-28 13:52:39 -0300

    fixing vscode number of spaces on tab

--- src/client/tcp_client.c
+++ src/client/tcp_client.c
@@ -8,40 +8,41 @@
 #define BUFFER_SIZE 65536
 
 HTTPResponse *shttp(HTTPRequest *request, char *host, unsigned int port) {
-    int sock;
-    struct sockaddr_in server_addr;
-    char buffer[BUFFER_SIZE];
-    int read_size;
-    HTTPResponse *response = NULL;
+  int sock;
+  struct sockaddr_in server_addr;
+  char buffer[BUFFER_SIZE];
+  int read_size;
+  HTTPResponse *response = NULL;
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        return NULL;
-    }
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock < 0) {
+    return NULL;
+  }
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = inet_addr(host);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  server_addr.sin_addr.s_addr = inet_addr(host);
 
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        close(sock);
-        return NULL;
-    }
-
-    strcpy(buffer, parse_request(request));
-    if (send(sock, buffer, strlen(buffer), 0) < 0) {
-        close(sock);
-        return NULL;
-    }
-
-    read_size = recv(sock, buffer, BUFFER_SIZE, 0);
-    if (read_size < 0) {
-        close(sock);
-        return NULL;
-    }
-
-    // TODO: Parse response to HTTPResponse
-
+  if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
     close(sock);
-    return response;
+    return NULL;
+  }
+
+  strcpy(buffer, parse_request(request));
+  if (send(sock, buffer, strlen(buffer), 0) < 0) {
+    close(sock);
+    return NULL;
+  }
+
+  read_size = recv(sock, buffer, BUFFER_SIZE, 0);
+  if (read_size < 0) {
+    close(sock);
+    return NULL;
+  }
+
+  // printf("%s\n", buffer);
+  // TODO: Parse response to HTTPResponse
+
+  close(sock);
+  return response;
 }
