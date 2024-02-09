0e25d54302c9c66936c71c00d88147156a728eaa
0x1eef <0x1eef@protonmail.com>
2024-02-09 18:06:27 -0300

    fixup! Add lib/aria_data_structures/ changes

--- include/httpclient.h
+++ include/httpclient.h
@@ -1,6 +1,6 @@
 #ifndef _ARIA_HTTP_CLIENT_H
 #define _ARIA_HTTP_CLIENT_H
-#include "../lib/aria_data_structures/datastructures.h"
+#include "../lib/aria_data_structures/include/datastructures.h"
 #include "../src/parse/request/parse_request.h"
 #include "../src/client/tcp_client.h"
 #endif // _ARIA_HTTP_CLIENT_H
